672e16e77d9af4088fcf36f59bcd37da4a72b6e4
Pankaj Sharma <pankajsharma0508@hotmail.com>
2024-04-15 21:36:42 +0200

    Added amplify

---
+++ amplify/amplify.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_amplify_app" "sfc_amplify_app" {
+  name                     = "sfc-amplify-app"
+  repository               = "https://github.com/pankajsharma0508/sfc"
+  access_token             = "..."
+  enable_branch_auto_build = true
+}
+
+resource "aws_amplify_branch" "sfc_amplify_branch_main" {
+  count       = 1
+  app_id      = aws_amplify_app.sfc_amplify_app.id
+  branch_name = "main"
+  enable_auto_build = true
+  stage       = "PRODUCTION"
+ 
+  environment_variables = {
+    Env = "prod"
+  }
+}
