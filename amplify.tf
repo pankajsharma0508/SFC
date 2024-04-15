135ed4e8cf961cfc2189349fa0390fbcda7831e4
Pankaj Sharma <pankajsharma0508@hotmail.com>
2024-04-15 22:05:13 +0200

    v2

--- amplify.tf
+++ amplify.tf
@@ -1,12 +1,14 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
 resource "aws_amplify_app" "sfc_amplify_app" {
   name                     = "sfc-amplify-app"
   repository               = "https://github.com/pankajsharma0508/sfc"
   access_token             = "..."
   enable_branch_auto_build = true
+
+   custom_rule {
+    source = "/<*>"
+    status = "200"
+    target = "/static/index.html<*>"
+  }
 }
 
 resource "aws_amplify_branch" "sfc_amplify_branch_main" {
